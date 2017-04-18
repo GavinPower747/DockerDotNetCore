9d3dd6046f1b2f13274810a68b95790c7bc02b6e
GavinPower747 <gavinpower747@hotmail.com>
2017-04-19 00:41:13 +0100

    Added app settings and made return message configurable

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace TestMvcApp.Controllers
 {
     public class HomeController
     {
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
         [HttpGet]
         public string Index(string message)
         {
-            return "Hello world from a docker container";
+            return _config["Strings:ReturnMessage"];
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -8,11 +9,23 @@ namespace TestMvcApp
 {
     public class Startup
     {
+        private IConfiguration Config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            Config = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange:true)
+                .AddEnvironmentVariables()
+                .Build();   
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton(Config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,13 +37,8 @@ namespace TestMvcApp
             {
                 app.UseDeveloperExceptionPage();
             }
-            
-            app.UseMvc(routes => 
-                routes.MapRoute(
-                    name: "DefaultRoute",
-                    template: "{controller=Home}/{action=Index}/{id?}"
-                )
-            );
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
